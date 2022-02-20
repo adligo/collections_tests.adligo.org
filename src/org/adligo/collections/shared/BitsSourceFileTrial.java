cac36c7b125067956f06e4fc029e561843fee09f
Scott Morgan <scott@adligo.com>
2022-02-19 19:02:38 -0600

    Added a fast way to count bits.

---
+++ src/org/adligo/collections/shared/BitsSourceFileTrial.java
@@ -0,0 +1,78 @@
+package org.adligo.collections.shared;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static org.adligo.i_ctx4jse.shared.I_PrintMixinStatic.*;
+import org.adligo.tests4j4jj.JjSourceFileTrial;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests of the CompressedIndexNodeMutant
+ * 
+ * @author scott
+ *
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
+public class BitsSourceFileTrial extends JjSourceFileTrial  {
+
+  public static void main(String [] args) {
+    Map<Integer,List<Byte>> bytesByCount = new HashMap<>();
+    for (byte i = Byte.MIN_VALUE; i < Byte.MAX_VALUE; i++) {    
+      int count = countBits(i);
+      List<Byte> bl = bytesByCount.get(count);
+      if (bl == null) {
+        bl = new ArrayList<>();
+        bytesByCount.put(count, bl);
+      } 
+      bl.add(i);
+    }
+    for (int i : bytesByCount.keySet()) {
+      List<Byte> bs = bytesByCount.get(i);
+      for (Byte b: bs) {
+        println("        case (byte) 0x" + String.format("%02X", b) + ": //no break");
+      }
+      println("          return " + i + ";");
+    }
+  }
+  
+  public static int countBits(byte b) {
+    int count = 0;
+    for (int i = 0; i < 8; i++) {
+      int set = b & (1 << i);
+      if (set != 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  @Test
+  public void testGetBitCount() {
+    for (byte i = Byte.MIN_VALUE; i < Byte.MAX_VALUE; i++) {    
+      int count = countBits(i);
+      equals(count, Bits.countBits(i));
+    }
+  }
+}
--- src/org/adligo/collections/shared/IndexParamsSourceFileTrial.java
+++ src/org/adligo/collections/shared/IndexParamsSourceFileTrial.java
@@ -8,13 +8,36 @@ import java.math.BigInteger;
 import org.adligo.tests4j4jj.JjSourceFileTrial;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests of the IndexParams
+ * 
+ * @author scott
+ *
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
 public class IndexParamsSourceFileTrial extends JjSourceFileTrial {
 
   public static void main(String [] args) {
     BigInteger maxInt = new BigInteger("" + Integer.MAX_VALUE);
     for (long i = 1; i < Integer.MAX_VALUE; i++) {
       BigInteger base = new BigDecimal(2).pow((int) i).toBigInteger();
-      //println("p.setBase(" + base + ");");
+      //println("p.setLeafBase(" + base + ");");
       if (base.bitLength() <= maxInt.bitLength()) {
       //rintln("public static final int BASE_" + base + " = " + base + ";");
       //println("case BASE_" + base + ": base2Exponent = " + i + "; break;");
@@ -31,6 +54,7 @@ public class IndexParamsSourceFileTrial extends JjSourceFileTrial {
   public void testSetBase() {
     IndexParams p = new IndexParams();
     p.setBase(2);
+    equals(1, p.getBase2Exponent());
     p.setBase(4);
     p.setBase(8);
     p.setBase(16);
@@ -60,8 +84,95 @@ public class IndexParamsSourceFileTrial extends JjSourceFileTrial {
     p.setBase(268435456);
     p.setBase(536870912);
     p.setBase(1073741824);
+    equals(30, p.getBase2Exponent());
     thrown(
         new IllegalArgumentException(IndexParams.THE_BASE + 3 + IndexParams.IS_NOT_SUPPORETED),
         () -> p.setBase(3));
+  }
+  
+  @Test
+  public void testSetBase2Exponent() {
+    IndexParams p = new IndexParams();
+    p.setBase2Exponent(1);
+    equals(1, p.getBase2Exponent());
+    equals(2, p.getBase());
+    
+    p.setBase2Exponent(30);
+    equals(30, p.getBase2Exponent());
+    equals(1073741824, p.getBase());
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 0 + IndexParams.IS_NOT_SUPPORETED),
+        () -> p.setBase2Exponent(0));
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 31 + IndexParams.IS_NOT_SUPPORETED),
+        () -> p.setBase2Exponent(31));
+  }
+  
+
+  @Test
+  public void testSetLeafBase() {
+    IndexParams p = new IndexParams();
+    p.setLeafBase(2);
+    equals(1, p.getLeafBase2Exponent());
+    p.setLeafBase(4);
+    p.setLeafBase(8);
+    p.setLeafBase(16);
+    p.setLeafBase(32);
+    p.setLeafBase(64);
+    p.setLeafBase(128);
+    p.setLeafBase(256);
+    p.setLeafBase(512);
+    p.setLeafBase(1024);
+    p.setLeafBase(2048);
+    p.setLeafBase(4096);
+    p.setLeafBase(8192);
+    p.setLeafBase(16384);
+    p.setLeafBase(32768);
+    p.setLeafBase(65536);
+    p.setLeafBase(131072);
+    p.setLeafBase(262144);
+    p.setLeafBase(524288);
+    p.setLeafBase(1048576);
+    p.setLeafBase(2097152);
+    p.setLeafBase(4194304);
+    p.setLeafBase(8388608);
+    p.setLeafBase(16777216);
+    p.setLeafBase(33554432);
+    p.setLeafBase(67108864);
+    p.setLeafBase(134217728);
+    p.setLeafBase(268435456);
+    p.setLeafBase(536870912);
+    p.setLeafBase(1073741824);
+    equals(30, p.getLeafBase2Exponent());
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE + 3 + IndexParams.IS_NOT_SUPPORETED),
+        () -> p.setLeafBase(3));
+  }
+  
+  @Test
+  public void testSetLeafBase2Exponent() {
+    IndexParams p = new IndexParams();
+    p.setLeafBase2Exponent(3);
+    equals(3, p.getBase2Exponent());
+    equals(8, p.getLeafBase());
+    
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 0 + IndexParams.IS_NOT_SUPPORETED),
+        () -> p.setLeafBase2Exponent(0));
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 31 + IndexParams.IS_NOT_SUPPORETED),
+        () -> p.setLeafBase2Exponent(31));
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 2 + IndexParams.IS_NOT_SUPPORETED_YET),
+        () -> p.setLeafBase2Exponent(2));
+    
+    thrown(
+        new IllegalArgumentException(IndexParams.THE_BASE_2_EXPONENT + 4 + IndexParams.IS_NOT_SUPPORETED_YET),
+        () -> p.setLeafBase2Exponent(4));
   }
 }
